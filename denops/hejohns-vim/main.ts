e0311272ee3cc0145d23409f90f7a3b41b0ad61a
hejohns <hejohns@umich.edu>
2025-01-31 04:47:43 -0500

    init deno

---
+++ denops/hejohns-vim/main.ts
@@ -0,0 +1,12 @@
+import type { Entrypoint, Denops } from "jsr:@denops/std";
+import * as batch from "jsr:@denops/std/batch";
+import * as fn from "jsr:@denops/std/function";
+import * as vars from "jsr:@denops/std/variable";
+import * as helper from "jsr:@denops/std/helper";
+
+export const main: Entrypoint = async (denops : Denops) => {
+    denops.dispatcher = {
+        async init(){
+        },
+    };
+};

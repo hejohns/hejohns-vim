dfb2a8330a368c959c8f696a55ea4798faf22d58
hejohns <hejohns@umich.edu>
2025-01-31 21:09:02 -0500

    I thought I had changed that already...

--- denops/hejohns-vim/main.ts
+++ denops/hejohns-vim/main.ts
@@ -25,8 +25,8 @@ export const main: Entrypoint = async (denops : Denops) => {
                           stdout: "piped",
                         });
                         const { stdout } = await date_cmd.output();
-                        vars.globals.set(denops, "myTime", new TextDecoder().decode(stdout).trim())
-                        vars.globals.set(denops, "myStatuslineUpdated", 1)
+                        vars.globals.set(denops, "hejohns#time", new TextDecoder().decode(stdout).trim())
+                        vars.globals.set(denops, "hejohns#statusline_updated", 1)
                     }, 5000);
                 }
                 else{
